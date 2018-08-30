b6b66d13e6f62eb6ee55ba423a30d1df4754d6f1
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-08-30 10:32:16 +0700

    email

--- app/Mail/UserEmail.php
+++ app/Mail/UserEmail.php
@@ -28,6 +28,6 @@ class UserEmail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.sendemail');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@
 |
 */
 
+Route::get('/send_email', array('uses' => 'EmailController@sendEmailReminder'));
